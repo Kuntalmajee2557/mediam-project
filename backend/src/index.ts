a2387c3467e675c4cf99866fb87e7f9cb8003faa
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2024-12-02 23:00:23 +0530

    cors added for frontend request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,8 +6,18 @@ import { userRouter } from './routes/user'
 import { useId } from 'hono/jsx'
 import { createPost, updatePost } from '@coderkuntl/common'
 import bookRouter from './routes/blog'
+import { cors } from 'hono/cors'
 
 const app = new Hono();
+
+app.use(
+	'*',
+	cors({
+	  origin: 'http://localhost:5173',
+	  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	  allowHeaders: ['Content-Type', 'Authorization'],
+	})
+  );
 
 app.get('/', (c) => {
 	return c.json({ id: "123" })
